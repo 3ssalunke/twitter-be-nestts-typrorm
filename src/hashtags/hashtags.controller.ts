87a6c8592cd53aaae3f9be6d2b87961194b9093e
surajs <3ssalunke>
2022-02-18 00:09:00 +0530

    modified readme and github actions

---
+++ src/hashtags/hashtags.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('hashtags')
+export class HashtagsController {
+  @Get('/')
+  getHashTags(): string {
+    return 'all hashtags';
+  }
+
+  @Get('/:tag/posts')
+  getPostsForHashtag(@Param() param): string {
+    return `all posts for hashtag ${param.tag}`;
+  }
+}
