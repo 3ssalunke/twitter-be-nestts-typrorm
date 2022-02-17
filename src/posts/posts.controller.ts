31ebbf53a36319fe5cca0bf31b2b3c9e7ba532f9
surajs <3ssalunke>
2022-02-18 01:03:46 +0530

    added all routes for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts() {
+    return 'get all posts';
+  }
+
+  @Get('/:postid')
+  getPostDetails(@Param() param) {
+    return `get post ${param.postid}`;
+  }
+
+  @Post('/')
+  createNewPost() {
+    return 'created new post';
+  }
+
+  @Delete('/:postid')
+  deletePost(@Param() param) {
+    return `deleting a post ${param.postid}`;
+  }
+
+  @Put('/:postid')
+  likePost(@Param() param) {
+    return `liked a post ${param.postid}`;
+  }
+
+  @Delete('/:postid')
+  unlikePost(@Param() param) {
+    return `unliked a post ${param.postid}`;
+  }
+}
