70c3d3860315dad756182b89254093606f9f89a9
surajs <3ssalunke>
2022-02-18 12:47:50 +0530

    Created User Entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,30 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("users")
-export class UserEntity {}
+export class UserEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ nullable: false, length: 30, unique: true })
+  username: string;
+
+  @Column({ nullable: true, length: 50 })
+  name: string;
+
+  @Column({ nullable: true })
+  avatar: string;
+
+  @Column({ nullable: true, length: 240 })
+  bio: string;
+
+  @Column({ name: "follower_count", default: 0 })
+  followerCount: number;
+
+  @Column({ name: "followee_count", default: 0 })
+  followeeCount: number;
+
+  @Column("boolean", { default: false })
+  verified: boolean;
+
+  userPassword: string;
+}
